a763f370c807500a8ad0b294f935f17523eef7d1
Maksimilian <shelekhovmn@gmail.com>
2023-02-01 07:56:08 +0900

    Initial Commit

---
+++ MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by Maksimilian Shelekhov
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,18 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Maksimilian Shelekhov
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
